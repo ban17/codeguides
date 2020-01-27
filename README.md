7d2e5f5989384e332ea5b9a563264c50c715d7be
zarko <zivojnovic@gmail.com>
2020-01-27 23:09:25 +0100

    other config files

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ run `npm i`
 run `npm run build`
 
 run `npm start`
+
+### Linting
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,17 @@
 import React, { useState } from 'react'
 
 const App = () => {
-    const [isOpen, changeIsOpen] = useState(false)
+  const [isOpen, changeIsOpen] = useState(false)
 
-    return (
-        <div className='app'>
-            <h1>Code Guides App</h1>
-            <button onClick={() => changeIsOpen(!isOpen)}>{ isOpen ? 'Hide current year' : 'Show current year' }</button>
-            {isOpen && <h2>{new Date().getFullYear()}</h2>}
-        </div>
-    )
+  return (
+    <div className='app'>
+      <h1>Code Guides App</h1>
+      <button onClick={() => changeIsOpen(!isOpen)}>
+        {isOpen ? 'Hide current year' : 'Show current year'}
+      </button>
+      {isOpen && <h2>{new Date().getFullYear()}</h2>}
+    </div>
+  )
 }
 
 export default App
